ac5a473af6991e74b084e1053f0c60bb39f8b207
berdikulov-571 <mygithub571@gmail.com>
2024-03-20 16:44:12 +0500

    ts file

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MainService } from '../main/services/main.service';
 import { Company } from '../main/models/Company';
+import { ContactService } from './services/contact.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-contact',
@@ -11,13 +13,33 @@ export class ContactComponent {
 
   companyList!: Company[];
 
-  constructor(private companyService: MainService) {
+  constructor(private companyService: MainService, private contactService: ContactService) {
     this.getAllCompany();
   }
 
   getAllCompany() {
     (this.companyService.getAllDestination().subscribe(response => {
       this.companyList = response;
+    }));
+  }
+
+  async createNotification() {
+    let InputName = document.querySelector('#inputName') as HTMLInputElement;
+    let InputEmail = document.querySelector('#inputEmail') as HTMLInputElement;
+    let InputPhoneNumber = document.querySelector('#inputPhoneNumber') as HTMLInputElement;
+    let InputSubject = document.querySelector('#inputSubject') as HTMLInputElement;
+    let InputComment = document.querySelector('#inputComment') as HTMLInputElement;
+
+    let formData = new FormData();
+    formData.append('clientName', InputName.value);
+    formData.append('clientPhone', InputPhoneNumber.value);
+    formData.append('clientEmail', InputEmail.value);
+    formData.append('subject', InputSubject.value);
+    formData.append('message', InputComment.value);
+
+    (this.contactService.createNotification(formData).subscribe(response => {
+      console.log(response);
+      location.reload();
     }));
   }
 
